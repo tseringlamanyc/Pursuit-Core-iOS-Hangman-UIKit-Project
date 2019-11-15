dfb4e43ecc10ee51aa0134f091beda0b93ba98d5
Tsering Lama <tseringlama@pursuit.org>
2019-11-14 22:55:38 -0500

    created 2 view controllers

---
+++ Hangman/Hangman/Player1VC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Alex Paul on 11/19/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class Player1VC: UIViewController {
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var player1Word: UILabel!
+    @IBOutlet weak var player1WordEnter: UITextField!
+    @IBOutlet weak var submitWord: UIButton!
+    
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+
+}
+
